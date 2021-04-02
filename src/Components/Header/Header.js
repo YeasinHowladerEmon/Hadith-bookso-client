a556e612e8bdf104c5f6981ce1e41040ebbb1152
emonibnsalim <emonibnsali@gmail.com>
2021-04-02 23:38:39 +0600

    addeed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 <>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-  <a class="navbar-brand" href="/">Navbar</a>
+  <a class="navbar-brand" href="/">ISLAMIC HADITH BOOKS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
